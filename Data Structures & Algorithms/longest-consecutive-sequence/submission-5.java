b02447f3dad9b69c87affb1c15c4a75e9aa969cd
Nathan Simon <nsimon06@uw.edu>
2026-05-14 12:01:50 -0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+
+        // for each num find the start of the seqeuence
+        // if start {
+        //     iterate through
+        // }
+        // else {
+        //     continue
+        // }
+
+
+        //add all elements to a set
+        Set<Integer> s = new HashSet();
+        for(int i : nums) {
+            s.add(i);
+        }
+
+        int max = 0;
+
+        //loop through each number in nums
+        for(int i : nums) {
+
+            //check whether its the start of a sequence
+            if(s.contains(i-1)) {
+                //not the start of a sequence
+                continue;
+            }
+
+            //is the start of a sequence
+            else {
+
+                //set the number we are looking for
+                int target = i+1;
+
+                //initialize curr sequence length as 1
+                int curr = 1;
+
+                //if s has target val incrememnt target and curr seq length
+                while(s.contains(target)) {
+                    curr++;
+                    target++;
+                }
+
+                //escaped loop, check if seq length > max
+                if(curr > max) {
+                    max = curr;
+                }
+            }
+        }
+
+        return max;
+    }
+}
