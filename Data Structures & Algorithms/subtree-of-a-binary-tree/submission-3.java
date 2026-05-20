18740ba7236bfe8f81304aff4e734f6b2883752b
Nathan Simon <nsimon06@uw.edu>
2026-05-19 22:47:28 -0700

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            if(curr == null) {
+                continue;
+            }
+
+            if(curr.val == subRoot.val && dfs(curr, subRoot)) {
+                return true;
+            }
+
+            q.add(curr.left);
+            q.add(curr.right);
+        }
+
+        return false;
+
+    }
+
+    private boolean dfs(TreeNode root, TreeNode subRoot) {
+
+        if(root == null && subRoot == null) {
+            return true;
+        }
+        
+        if(root == null || subRoot == null) {
+            return false;
+        }
+
+        if(root.val != subRoot.val) {
+            return false;
+        }
+
+        return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
+    }
+}
+
+
+/*
+
+    bfs until common root found
+
+    dfs from there?
+
+*/
