dccfc813abfdf8fc0bd27746235870d911d365f9
Nathan Simon <nsimon06@uw.edu>
2026-05-29 23:47:00 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        backtrack(nums, 0, ans, curr);
+
+        return ans;
+    }
+
+    private void backtrack(int[] nums, int i, List<List<Integer>> ans, List<Integer> curr) {
+
+        //base case: i == nums.length - 1
+        if(i == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        //decision tree
+        
+        // don't add
+        backtrack(nums, i + 1, ans, curr);
+
+        //add
+        curr.add(nums[i]);
+        backtrack(nums, i + 1, ans, curr);
+
+        //remove
+        if(curr.size() > 0) {
+            curr.remove(curr.size() - 1);
+        }
+
+        // curr.add(nums[i]);
+        // backtrack(nums, i + 1, ans, curr);
+        // curr.remove(curr.size() - 1);
+
+        // backtrack(nums, i + 1, ans, curr);
+        
+        
+    }
+}
